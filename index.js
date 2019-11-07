238826127a4ba854d70847da9bf716c13212005c
Hristian Kanev <hristiankanev@gmail.com>
2019-11-07 16:23:28 +0200

    bot 2.0

--- index.js
+++ index.js
@@ -2,62 +2,63 @@ require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
 var moment = require('moment');
-const JsonDB = require('node-json-db').JsonDB;
-const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
+const { Client } = require('pg');
+var _ = require('lodash');
 
-var db = new JsonDB(new Config("archeage", true, false, '/'));
+const dbClient = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
-// db.push('/chars', {
-//   Labrys: {labor: 0, date: null},
-//   Mutant: {labor: 0, date: null},
-//   Hadley: {labor: 0, date: null},
-//   Klarion: {labor: 0, date: null},
-//   Eonic: {labor: 0, date: null},
-//   Dazzler: {labor: 0, date: null},
-//   Vue: {labor: 0, date: null},
-//   Kekambas: {labor: 0, date: null},
-//   Atami: {labor: 0, date: null},
-//   LittleLady: {labor: 0, date: null},
-// }, true);
+dbClient.connect();
 
-client.on('ready', () => {
+var chars = null;
+
+async function getChars(){
+  let response =  await dbClient.query('SELECT * FROM public.chars');
+  return response.rows;
+}
+
+
+var chars = getChars();
+
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', async msg => {
-  var chars = db.getData("/chars");
   if(msg.channel.name === 'chars'){
     if (msg.content.toLowerCase().includes('!set')) {
       let [command, char, labor] = msg.content.split(' ');
-      char = char[0].toUpperCase()+ char.slice(1).toLowerCase();
+      char = char.toLowerCase();
 
       if(labor < 0 ){
         labor = 0;
       }
 
-      chars[char].labor = labor;
-      chars[char].date = moment();
-      db.push('/chars', chars);
-      await msg.channel.bulkDelete(10);
-      msg.reply(`Saved!`).then(msg => {
-        msg.delete(1000);
-      });
+      var query = "UPDATE public.chars SET labor = ($1), updated_at = ($2) WHERE system_name = ($3)";
+      await dbClient.query(query, [labor, moment(), char]).then(async () => {
+            await msg.channel.bulkDelete(10);
+            msg.reply(`Saved!`).then(msg => {
+              msg.delete(1000);
+            });
+          }
+      );
     }
     if(msg.content === '!status'){
       const embed = new Discord.RichEmbed()
           .setTitle("Character summary")
           .setColor(0x00AE86)
 
-      Object.keys(chars).forEach(char => {
-        var diffMins =   moment().diff(chars[char].date, 'minutes')
-
+      var chars = await getChars();
+      chars.forEach(char => {
+        var diffMins = moment().diff(moment(char.updated_at), 'minutes');
         var accumulatedIterations = Math.round(diffMins / 5);
-
-        var accumulatedLabor =  parseInt(chars[char].labor) + (10 * accumulatedIterations);
-
+        var accumulatedLabor =  parseInt(char.labor) + (10 * accumulatedIterations);
         accumulatedLabor = accumulatedLabor > 5000  ? 5000 : accumulatedLabor;
-        embed.addField(char, `Generated Labor: ${accumulatedLabor}`, true)
+        embed.addField(char.name, `Generated Labor: ${accumulatedLabor}`, true)
       })
+
       await msg.channel.bulkDelete(10);
       msg.reply({embed});
     }
